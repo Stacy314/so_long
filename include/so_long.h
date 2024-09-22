e71bc7f964ba549420053dcd135662159cfa3bc5
Anastasiia <nestea413@gmail.com>
2024-09-22 22:50:52 +0200

    doesnt work

--- include/so_long.h
+++ include/so_long.h
@@ -16,6 +16,14 @@
 # define D_KEY 2
 # define ESC_KEY 53
 
+// константи для напрямків руху
+# define UP 0
+# define DOWN 1
+# define LEFT 2
+# define RIGHT 3
+
+
+
 typedef struct s_game {
     void    *mlx;
     void    *win;
--- src/map.c
+++ src/map.c
@@ -3,15 +3,18 @@
 int parse_map(t_game *game, char *filename)
 {
     int fd;
-    char *line;
+    // char *line; // Коментуємо або видаляємо цю змінну, поки не буде потрібна
+
+    (void)game; // Додаємо це, щоб тимчасово ігнорувати попередження про невикористаний параметр
 
     fd = open(filename, O_RDONLY);
     if (fd < 0)
         return (0);
-    // Тут код для парсингу карти з файлу
+    // Тут буде код для парсингу карти
     close(fd);
     return (1);
 }
+
 
 int init_game(t_game *game, char *map_file)
 {
